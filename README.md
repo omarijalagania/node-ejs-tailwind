f7d4c254ca1466d1539ca783d9959a7ee62f4d88
omari <omari.jalagania@gmail.com>
2022-05-13 01:06:20 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+1. to start watch changes in tailwind css, fist start : npm run watch
+2. than start npm start
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["**/*.html", "views/*.ejs", "**/*.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
