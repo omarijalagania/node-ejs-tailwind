a1bed63b072fc49640db2c8fbec0be0b9d7f157f
omari <omari.jalagania@gmail.com>
2022-05-16 01:52:13 +0400

    chore: remove unused validation

--- schema/validation.js
+++ schema/validation.js
@@ -4,16 +4,6 @@ const validateLogin = (data) => {
   const schema = Joi.object({
     email: Joi.string().min(6).required().email(),
     password: Joi.string().min(6).required(),
-    cart: Joi.object({
-      items: Joi.array().items(
-        Joi.object({
-          title: Joi.string(),
-          description: Joi.string(),
-          price: Joi.string(),
-          image: Joi.string(),
-        }),
-      ),
-    }),
   })
 
   return schema.validate(data)
