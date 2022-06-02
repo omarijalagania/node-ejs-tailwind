14f6ed0b228990eb470120d3a029abaaabffd49f
omari <omari.jalagania@gmail.com>
2022-06-02 18:48:23 +0400

    feat: add filetype image

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const indexRouter = require("./routes/index")
 const adminRouter = require("./routes/admin")
 const userAuth = require("./routes/auth")
 const app = express()
-const upload = require("./utils/image-upload")
+const fileUpload = require("./utils/image-upload")
 const uploadRoute = require("./routes/upload")
 // view engine setup
 app.set("views", path.join(__dirname, "views"))
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, "public")))
 app.use("/", indexRouter.login)
 app.use("/admin", adminRouter.dashboard)
 app.use("/user", userAuth.authUser)
-app.use("/file", upload.single("image"), uploadRoute.upload)
+app.use("/file", fileUpload.upload.single("image"), uploadRoute.upload)
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
--- utils/image-upload.js
+++ utils/image-upload.js
@@ -2,14 +2,25 @@ const multer = require("multer")
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/uploads")
+    cb(null, "public/images")
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9)
     cb(null, file.fieldname + "-" + file.originalname)
   },
 })
 
-const upload = multer({ storage: storage })
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg"
+  ) {
+    cb(null, true)
+  } else {
+    cb(null, false)
+  }
+}
 
-module.exports = upload
+const upload = multer({ storage: storage, fileFilter: fileFilter })
+
+exports.upload = upload
